a3627b12b77ec91e59433ea6998aae25fdf71946
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-10-16 12:38:59 -0400

    feat: add list song to state
    closes: #1

--- lib/add_song/cubit/add_song_state.dart
+++ lib/add_song/cubit/add_song_state.dart
@@ -1,24 +1,27 @@
 part of 'add_song_cubit.dart';
 
 class AddSongState extends Equatable {
-  const AddSongState({
-    this.link = const Link.pure(),
-    this.status = FormzStatus.pure,
-  });
+  const AddSongState(
+      {this.link = const Link.pure(),
+      this.status = FormzStatus.pure,
+      this.songs = const []});
 
   final Link link;
   final FormzStatus status;
+  final List<Song> songs;
 
   @override
-  List<Object> get props => [link, status];
+  List<Object> get props => [link, status, songs];
 
   AddSongState copyWith({
     Link? link,
     FormzStatus? status,
+    List<Song>? songs,
   }) {
     return AddSongState(
       link: link ?? this.link,
       status: status ?? this.status,
+      songs: songs ?? this.songs,
     );
   }
 }
